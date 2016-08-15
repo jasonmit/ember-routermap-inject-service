26fb793f8072472ec681a35eb6e3eddfc40db88a
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-08-14 21:56:56 -0700

    Reimplement to not extend the prototype for each instance initialized

--- addon/dsl.js
+++ addon/dsl.js
@@ -1,9 +1,42 @@
 import Ember from 'ember';
 
-export function setup(instance) {
-  const proto = Ember.RouterDSL.prototype;
+const { Router, getOwner } = Ember;
+
+export function setup() {
+  let isEnabled = Ember.__loader.require('ember-metal/features').default;
+  let proto = Ember.RouterDSL.prototype;
+
+  Router.reopen({
+    _buildDSL() {
+      let moduleBasedResolver = this._hasModuleBasedResolver();
+      let owner = getOwner(this);
+
+      let options = {
+        owner: owner,
+        enableLoadingSubstates: !!moduleBasedResolver
+      };
+
+      if (isEnabled('ember-application-engines')) {
+        let router = this;
+
+        options.enableLoadingSubstates = !!moduleBasedResolver;
+
+        options.resolveRouteMap = function(name) {
+          return owner._lookupFactory('route-map:' + name);
+        };
+
+        options.addRouteForEngine = function(name, engineInfo) {
+          if (!router._engineInfoByRoute[name]) {
+            router._engineInfoByRoute[name] = engineInfo;
+          }
+        };
+      }
+
+      return new Ember.RouterDSL(null, options);
+    }
+  });
 
   proto.service = function service(serviceName) {
-    return instance.lookup('service:' + serviceName);
+    return this.options.owner.lookup('service:' + serviceName);
   };
 }
