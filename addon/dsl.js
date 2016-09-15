bfd64241a4700b4ddef1b26d0b5123484a16ccb1
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-09-14 20:45:55 -0700

    Update dsl.js

--- addon/dsl.js
+++ addon/dsl.js
@@ -3,32 +3,16 @@ import Ember from 'ember';
 const { Router, getOwner } = Ember;
 
 export function setup() {
-  Router.reopen({
-    _buildDSL() {
-      let moduleBasedResolver = this._hasModuleBasedResolver();
-      let owner = getOwner(this);
-      let router = this;
-
-      let options = {
-        owner: owner,
-        enableLoadingSubstates: !!moduleBasedResolver
-      };
-
-      options.resolveRouteMap = function(name) {
-        return owner._lookupFactory('route-map:' + name);
-      };
-
-      options.addRouteForEngine = function(name, engineInfo) {
-        if (!router._engineInfoByRoute[name]) {
-          router._engineInfoByRoute[name] = engineInfo;
-        }
-      };
-
-      return new Ember.RouterDSL(null, options);
-    }
-  });
-
   Ember.RouterDSL.prototype.service = function service(serviceName) {
     return this.options.owner.lookup('service:' + serviceName);
   };
+
+  Router.reopen({
+    _buildDSL() {
+      const dsl = this._super.apply(this, arguments);
+      dsl.options.owner = getOwner(this);
+
+      return dsl;
+    }
+  });
 }
