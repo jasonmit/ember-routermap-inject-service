721fa6a497d0e583ecb5e42d999193d29dfe3319
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-09-14 20:50:23 -0700

    Update dsl.js

--- addon/dsl.js
+++ addon/dsl.js
@@ -4,13 +4,13 @@ const { Router, getOwner } = Ember;
 
 export function setup() {
   Ember.RouterDSL.prototype.service = function service(serviceName) {
-    return this.options.owner.lookup('service:' + serviceName);
+    return this.__owner.lookup('service:' + serviceName);
   };
 
   Router.reopen({
     _buildDSL() {
       const dsl = this._super.apply(this, arguments);
-      dsl.options.owner = getOwner(this);
+      dsl.__owner = getOwner(this);
 
       return dsl;
     }
